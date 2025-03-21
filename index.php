f0cc9378d1046a6d92811cbc057158437143e4b8
AgustinMelgar07 <agustin0alberto7melgar@gmail.com>
2025-03-20 21:13:52 -0300

    Primer commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <form action="index.php" method="post">
+
+    Ingresa tu nombre: <input type="text" name="nombre" id="">
+    <br>
+    <input type="submit" value="Enviar">
+
+
+
+    </form>
+
+    <?php
+       
+
+    if(isset($_POST["nombre"])){
+
+        echo "</br>  Hola " .$_POST["nombre"] ;
+
+    }
+       
+    $v=0;
+
+    /*
+    for ($i=0; $i<20; $i++){
+        echo $v++ . "</br>";
+
+
+    }
+    
+    */
+    
+    ?>
+
+    
+
+</body>
+</html>
